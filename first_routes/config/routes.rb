0bc64b94aa5d38951cfff239d148fa64cda14fb7
dan <mr.danlay@gmail.com>
2022-08-24 14:51:36 -0400

    add manual routes

--- first_routes/config/routes.rb
+++ first_routes/config/routes.rb
@@ -3,9 +3,12 @@ Rails.application.routes.draw do
   # resources :users
   # Defines the root path route ("/")
   # root "articles#index"
-  get "/users", to: "users#index"
-  get "/users/:id", to: "users#show"
-  post "/users/:id", to: "users#create"
-  patch "/users/:id", to: "users#update"
-  delete "/users/:id", to: "users#destroy"
+  get '/users', to: 'users#index', as: 'users'
+  get '/users/:id', to: 'users#show', as: 'user'
+  post '/users/:id', to: 'users#create'
+  patch '/users/:id', to: 'users#update'
+  delete '/users/:id', to: 'users#destroy'
+  get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
+  put '/users/:id', to: 'users#update'
+  get '/users/:id/new', to: 'users#new', as: 'new_user'
 end
